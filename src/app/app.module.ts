fc384d5c901b6370c902372aaa9ad7c9eeb3b0cd
unknown <godramg@gmail.com>
2025-02-02 17:22:09 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CardFormComponent],
+  imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/card-form/card-form.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
+
+export const forbiddenNameValidator = (
+  forbiddenNames: string[]
+): ValidatorFn => {
+  return (control: AbstractControl): ValidationErrors | null => {
+    return forbiddenNames.includes(control.value)
+      ? { forbiddenName: 'Name is not allowed' }
+      : null;
+  };
+};
+
+@Component({
+  selector: 'app-card-form',
+  standalone: false,
+
+  templateUrl: './card-form.component.html',
+  styleUrl: './card-form.component.css',
+})
+export class CardFormComponent {
+  cardForm = new FormGroup({
+    name: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(5),
+        forbiddenNameValidator(['foo', 'bar']),
+      ],
+    }),
+  });
+}
